89c4b208e8438c0c5f86b9cea8d45308e4585d8d
gringrape <gringrape200@gmail.com>
2024-04-11 01:54:05 +0900

    ff9c92b0-c4a9-4cc3-a16d-b18e6e197fa9

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+def calculate(num1, num2, operation):
+    if operation == '+':
+        return num1 + num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == '/':
+        return num1 / num2
+
 def main():
     num1 = int(input("첫 번째 숫자를 입력하세요: "))
     num2 = int(input("두 번째 숫자를 입력하세요: "))
     operation = input("사칙연산을 선택하세요 (+, -, *, / 중 하나를 입력): ")
-
-    if operation == '+':
-        print(num1 + num2)
-    elif operation == '-':
-        print(num1 - num2)
-    elif operation == '*':
-        print(num1 * num2)
-    elif operation == '/':
-        print(num1 / num2)
+    result = calculate(num1, num2, operation)
+    print(result)
 
 main()
