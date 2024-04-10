88ee16972b92d355571d9ae295f13295b42b21df
gringrape <gringrape200@gmail.com>
2024-04-11 08:20:44 +0900

    Refactored Calculator Code
    Refactored the calculator code to evaluate the user input operation using the eval() function and provided a more informative output message.

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-def main():
+def calculate():
     num1 = int(input("첫 번째 숫자를 입력하세요: "))
     num2 = int(input("두 번째 숫자를 입력하세요: "))
     operation = input("사칙연산을 선택하세요 (+, -, *, / 중 하나를 입력): ")
 
-    if operation == '+':
-        print(num1 + num2)
-    elif operation == '-':
-        print(num1 - num2)
-    elif operation == '*':
-        print(num1 * num2)
-    elif operation == '/':
-        print(num1 / num2)
+    if operation in ['+', '-', '*', '/']:
+        result = eval(str(num1) + operation + str(num2))
+        print(f"결과: {result}")
+    else:
+        print("유효한 연산자를 입력하세요.")
 
-main()
+calculate()
