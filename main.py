e4954699b715f8bdacb89fb69ece3ff0a58d7f8e
Elly <elly@elements.im>
2024-04-11 13:24:38 +0900

    [Elly] Support for All Arithmetic Operations
    사칙연산을 모두 다룰 수 있게 구현합니다.

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 def main():
     num1 = int(input('첫 번째 숫자를 입력해주세요: '))
     num2 = int(input('두 번째 숫자를 입력해주세요: '))
-    result = num1 + num2
-    print('두 숫자의 합은:', result)
+    operation = input('어떤 연산을 하시겠습니까? (+, -, *, /): ')
+    if operation == '+':
+        result = num1 + num2
+        print('두 숫자의 합은:', result)
+    elif operation == '-':
+        result = num1 - num2
+        print('두 숫자의 차는:', result)
+    elif operation == '*':
+        result = num1 * num2
+        print('두 숫자의 곱은:', result)
+    elif operation == '/':
+        result = num1 / num2
+        print('두 숫자의 나눗셈 결과는:', result)
+    else:
+        print('잘못된 연산자 입력입니다.')
 
 main()
