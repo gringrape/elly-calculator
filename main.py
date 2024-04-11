29b1c477c38b66fd87fef331514d70b516d23672
gringrape <gringrape200@gmail.com>
2024-04-11 18:19:27 +0900

    test CI

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_sum(num1, num2):
-    return num1 + num2
-
 def main():
-    num1 = int(input('첫 번째 숫자를 입력하세요: '))
-    num2 = int(input('두 번째 숫자를 입력하세요: '))
-    result = calculate_sum(num1, num2)
-    print(f'{num1}와 {num2}의 합은 {result}입니다.')
+    print("hello, world")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- test_main.py
+++
@@ -1,8 +0,0 @@
-from main import calculate_sum
-
-def test_calculate_sum():
-    assert calculate_sum(3, 2) == 5, 'Expected sum of 3 and 2 to be 5'
-    assert calculate_sum(-1, 1) == 0, 'Expected sum of -1 and 1 to be 0'
-    assert calculate_sum(0, 0) == 0, 'Expected sum of 0 and 0 to be 0'
-    assert calculate_sum(-5, 6) == 1, 'Expected sum of -5 and 6 to be 1'
-    assert calculate_sum(100, 200) == 300, 'Expected sum of 100 and 200 to be 300'
