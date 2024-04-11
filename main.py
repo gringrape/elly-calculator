9a37cf06f8d4f74368d587f39d7c53dee3e7a3ba
Elly <elly@elements.im>
2024-04-11 13:23:42 +0900

    [Elly] 간단한 두 숫자 더하기 계산기
    두 개의 숫자를 입력받아 더한 결과를 출력하는 간단한 더하기 계산기를 만듭니다.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def main():
-    print("hello, world")
-
+    num1 = int(input('첫 번째 숫자를 입력해주세요: '))
+    num2 = int(input('두 번째 숫자를 입력해주세요: '))
+    result = num1 + num2
+    print('두 숫자의 합은:', result)
 
 main()
